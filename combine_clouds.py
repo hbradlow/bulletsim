0c5d60d08b8387f99da336ab8bacee88f809cab2
Henry Bradlow <hbradlow@berkeley.edu>
2012-10-12 13:15:50 -0700

    Working on cloud combine icp

--- combine_clouds.py
+++ combine_clouds.py
@@ -10,40 +10,57 @@ def sort_key(file):
 base_dir = "pcd_files2"
 files = sorted(os.listdir(base_dir),key=sort_key)
 args = ""
-for f in files[0::50]:
+for f in files[0::100]:
     args += base_dir + "/" + f + " "
 command = "./bin/test_cloud_reconstruction " + args
-if len(command)>6000:
+if len(command)>600:
     print command[0:600] + "......(more args)"
 else:
     print command
 
 #run the command
 proc = subprocess.Popen(command.strip().split(" "),stdout=subprocess.PIPE)
-points = []
+translations = []
 poses = []
+xs = []
+ys = []
+zs = []
 while True:
     line = proc.stdout.readline()
     print line
     if line == "":
         break;
-    if "Transform" in line:
+    if "Transform:" in line:
         #extract the list of poses from the output
         match = re.match("^Transform:\s*(.+?)\s+(.+?)\s+(.+?)\s*$",line)
-        points.append((match.group(1),match.group(2),match.group(3)))
-    if "Pose" in line:
+        translations.append((match.group(1),match.group(2),match.group(3)))
+    if "X:" in line:
         #extract the list of poses from the output
-        match = re.match("^Pose:\s*(.+?)\s+(.+?)\s+(.+?)\s*$",line)
-        poses.append((match.group(1),match.group(2),match.group(3)))
+        match = re.match("^X:\s*(.+?)\s+(.+?)\s+(.+?)\s*$",line)
+        xs.append((match.group(1),match.group(2),match.group(3)))
+    if "Y:" in line:
+        #extract the list of poses from the output
+        match = re.match("^Y:\s*(.+?)\s+(.+?)\s+(.+?)\s*$",line)
+        ys.append((match.group(1),match.group(2),match.group(3)))
+    if "Z:" in line:
+        #extract the list of poses from the output
+        match = re.match("^Z:\s*(.+?)\s+(.+?)\s+(.+?)\s*$",line)
+        zs.append((match.group(1),match.group(2),match.group(3)))
         
-#plot the points
-print points
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-fig = plt.figure()
-ax = fig.add_subplot(111,projection="3d")
-x = [float(a[0]) for a in points]
-y = [float(a[1]) for a in points]
-z = [float(a[2]) for a in points]
-ax.scatter(x,y,z)
-plt.show()
+def plot(points,label):
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    fig = plt.figure(label)
+    ax = fig.add_subplot(111,projection="3d")
+    x = [float(a[0]) for a in points]
+    y = [float(a[1]) for a in points]
+    z = [float(a[2]) for a in points]
+    ax.scatter(x,y,z)
+    plt.show()
+
+debug = False
+if debug:
+    plot(translations,"Translations")
+    plot(xs,"Affect on X axis")
+    plot(ys,"Affect on Y axis")
+    plot(zs,"Affect on Z axis")
--- src/tests/test_cloud_reconstruction.cpp
+++ src/tests/test_cloud_reconstruction.cpp
@@ -3,6 +3,8 @@
 #include <math.h>
 
 #include <pcl/registration/icp.h>
+#include <pcl/registration/icp_nl.h>
+#include <pcl/registration/transformation_estimation_point_to_plane_lls.h>
 #include "clouds/utils_pcl.h"
 
 #include <pcl/filters/filter_indices.h>
@@ -32,27 +34,83 @@ using namespace std;
 #define Y_UPPER_BOUND           0.1
 #define VERTICAL_LOWER_BOUND    0.01
 
-int loadColorCloud(char * filename,ColorCloudPtr cloud){
-    pcl::PointCloud<pcl::PointXYZRGBA> tmp;
-    if (pcl::io::loadPCDFile(filename, tmp) == -1) //* load the file
+//size of leaf to use for downsampling
+#define LEAF_SIZE               0.002f
+
+void getColorCloudFromPointNormalCloud(pcl::PointCloud<pcl::PointNormal>::Ptr a, ColorCloudPtr b){
+    b->clear();
+    for(int i = 0; i<a->size(); i++){
+        ColorPoint c;
+        pcl::PointNormal p = a->points[i];
+        c.x = p.x;
+        c.y = p.y;
+        c.z = p.z;
+        c.r = 255;
+        c.g = 255;
+        c.b = 255;
+        b->push_back(c);
+    }
+    b->width = a->width;
+    b->height = a->height;
+}
+int loadCloud(char * filename,pcl::PointCloud<pcl::PointNormal>::Ptr cloud,ColorCloudPtr color_cloud){
+    //load a pcd and convert it into a ColorCloud and a PointNormal cloud
+    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr tmp (new pcl::PointCloud<pcl::PointXYZRGBA>);
+    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr tmp_filtered (new pcl::PointCloud<pcl::PointXYZRGBA>);
+    if (pcl::io::loadPCDFile(filename, *tmp) == -1) //* load the file
     {
         PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
         return -1;
     }
+    //do some initial filtering by rejecting points too far from the camera
+    pcl::PassThrough<pcl::PointXYZRGBA> pass;
+    pass.setInputCloud (tmp);
+    pass.setFilterFieldName ("z");
+    pass.setFilterLimits (0, 1.2);
+    pass.setKeepOrganized(true);
+    pass.filter (*tmp_filtered);
+    *tmp = *tmp_filtered;
+
+    pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
+    //calculate normals for the cloud - a very slow part of the process
+    /*
+    pcl::NormalEstimation<pcl::PointXYZRGBA, pcl::Normal> ne;
+    ne.setInputCloud (tmp);
+    pcl::search::KdTree<pcl::PointXYZRGBA>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGBA>);
+    ne.setSearchMethod (tree);
+    ne.setRadiusSearch (0.01);
+    ne.compute (*normals);
+    */
+
     cloud->clear();
-    for(int i = 0; i<tmp.points.size(); i++){
-        pcl::PointXYZRGBA p1 = tmp.points[i];
-        ColorPoint p2;
+    color_cloud->clear();
+    for(int i = 0; i<tmp->points.size(); i++){
+        pcl::PointXYZRGBA p1 = tmp->points[i];
+        pcl::Normal n1 = normals->points[i];
+        n1.normal_x = 0;
+        n1.normal_y = 0;
+        n1.normal_z = 1;
+        pcl::PointNormal p2;
+        ColorPoint p3;
         p2.x = p1.x;
         p2.y = p1.y;
         p2.z = p1.z;
-        p2.r = p1.r;
-        p2.g = p1.g;
-        p2.b = p1.b;
+        p3.x = p1.x;
+        p3.y = p1.y;
+        p3.z = p1.z;
+        p2.normal_x = n1.normal_x;
+        p2.normal_y = n1.normal_y;
+        p2.normal_z = n1.normal_z;
+        p3.r = p1.r;
+        p3.g = p1.g;
+        p3.b = p1.b;
         cloud->push_back(p2);
+        color_cloud->push_back(p3);
     }
-    cloud->width = tmp.width;
-    cloud->height = tmp.height;
+    cloud->width = tmp->width;
+    cloud->height = tmp->height;
+    color_cloud->width = tmp->width;
+    color_cloud->height = tmp->height;
     return 1;
 }
 Eigen::Vector3f fitPlaneToTransformTranslations(vector<Eigen::Matrix4f> transforms,vector<int> mask){
@@ -122,12 +180,53 @@ Eigen::Matrix4f getSmoothedTransform(vector<Eigen::Matrix4f> transforms, int ind
     }
     return transform;
 }
-void closeCloud(ColorCloudPtr master){
+int getTransformForICPAlignment(pcl::PointCloud<pcl::PointNormal>::Ptr master, pcl::PointCloud<pcl::PointNormal>::Ptr current, Eigen::Matrix4f* transform){
+
+    pcl::PointCloud<pcl::PointNormal>::Ptr master_filtered (new pcl::PointCloud<pcl::PointNormal>);
+    pcl::PointCloud<pcl::PointNormal>::Ptr current_filtered (new pcl::PointCloud<pcl::PointNormal>);
+    pcl::PointCloud<pcl::PointNormal>::Ptr master_filtered_nonan (new pcl::PointCloud<pcl::PointNormal>);
+    pcl::PointCloud<pcl::PointNormal>::Ptr current_filtered_nonan (new pcl::PointCloud<pcl::PointNormal>);
+    vector<int> index;
+    pcl::removeNaNFromPointCloud(*current,index);
+    pcl::removeNaNFromPointCloud(*master,index);
+
+    /*
+    pcl::VoxelGrid<pcl::PointNormal> vg;
+
+    vg.setInputCloud (master_filtered_nonan);
+    vg.setLeafSize (LEAF_SIZE,LEAF_SIZE,LEAF_SIZE);
+    vg.filter (*master_filtered);
+
+    vg.setInputCloud (current_filtered_nonan);
+    vg.setLeafSize (LEAF_SIZE,LEAF_SIZE,LEAF_SIZE);
+    vg.filter (*current_filtered);
+
+    pcl::removeNaNFromPointCloud(*current_filtered,*current_filtered,index);
+    pcl::removeNaNFromPointCloud(*master_filtered,*master_filtered,index);
+    */
+
+    if(current_filtered->size()>0 && master_filtered->size()>0){
+        pcl::IterativeClosestPointNonLinear<pcl::PointNormal, pcl::PointNormal> icp; 
+        typedef pcl::registration::TransformationEstimationPointToPlaneLLS<pcl::PointNormal, pcl::PointNormal> PointToPlane;
+        boost::shared_ptr<PointToPlane> point_to_plane(new PointToPlane);
+        icp.setTransformationEstimation(point_to_plane);
+        icp.setInputCloud(current);
+        icp.setInputTarget(master);
+        pcl::PointCloud<pcl::PointNormal> Final;
+        icp.align(Final);
+        if(icp.hasConverged()){
+            *transform = icp.getFinalTransformation();
+            return 1;
+        }
+    }
+    return 0;
+}
+void closeCloud(pcl::PointCloud<pcl::PointNormal>::Ptr master){
     //closes the cloud by adding a plate at the bottom
-    ColorCloudPtr plate (new ColorCloud);
+    pcl::PointCloud<pcl::PointNormal>::Ptr plate (new pcl::PointCloud<pcl::PointNormal>);
 
     //get the ring of points at the bottom of the cloud
-    pcl::PassThrough<ColorPoint> pass;
+    pcl::PassThrough<pcl::PointNormal> pass;
     pass.setInputCloud (master);
     pass.setFilterFieldName ("z");
     pass.setFilterLimits (VERTICAL_LOWER_BOUND, VERTICAL_LOWER_BOUND+.05);
@@ -136,7 +235,7 @@ void closeCloud(ColorCloudPtr master){
     //take slices of the ring and add points to fill in the cloud
     for(float x = X_LOWER_BOUND; x<=X_UPPER_BOUND; x+=.001){
         //get just the stip contained by this slice
-        ColorCloud strip;
+        pcl::PointCloud<pcl::PointNormal> strip;
         pass.setInputCloud (plate);
         pass.setFilterFieldName ("x");
         pass.setFilterLimits (x, x+.01);
@@ -159,26 +258,26 @@ void closeCloud(ColorCloudPtr master){
         }
         //add points from ymin to ymax for this strip
         for(float y = min; y<=max; y+=.001){
-            ColorPoint p;
+            pcl::PointNormal p;
             p.x = x;
             p.y = y;
             p.z = VERTICAL_LOWER_BOUND;
-            p.r = 255;
-            p.g = 255;
-            p.b = 255;
             master->push_back(p);
         }
     }
 }
 
 int main(int argc, char*argv[]){
-    ColorCloudPtr current (new ColorCloud); //buffer to hold the cloud for the current frame
-    ColorCloudPtr master (new ColorCloud); //buffer to hold the concatenated point cloud of all frames
-    ColorCloudPtr filtered (new ColorCloud); //buffer to place filtered intermediate clouds
+    ColorCloudPtr tmp (new ColorCloud); //a buffer to store the cloud that is loaded from disk
+    pcl::PointCloud<pcl::PointNormal>::Ptr current (new pcl::PointCloud<pcl::PointNormal>); //buffer to hold the cloud for the current frame
+
+    pcl::PointCloud<pcl::PointNormal>::Ptr master (new pcl::PointCloud<pcl::PointNormal>); //buffer to hold the concatenated point cloud of all frames
+    pcl::PointCloud<pcl::PointNormal>::Ptr filtered (new pcl::PointCloud<pcl::PointNormal>);//buffer to place filtered intermediate clouds
+
 
     //keep track of all the frames
     std::vector<Eigen::Matrix4f> transforms; //the transform of the checkerboard for the frame
-    std::vector<ColorCloud> clouds; //the cloud for a frame
+    std::vector< pcl::PointCloud<pcl::PointNormal> > clouds; //the cloud for a frame
     std::vector<int> success_mask; //indicates if a checkerboard was located in the frame
 
     if(argc>1){
@@ -194,17 +293,19 @@ int main(int argc, char*argv[]){
         for(int i=1; i<argc; i++)
         {
             //load the cloud from the file
-            if(loadColorCloud(argv[i],current) == -1)
+            if(loadCloud(argv[i],current,tmp) == -1)
                 return -1;
+            
             clouds.push_back(*current);
+
             total_clouds ++;
             //update the user of the progress
-            if(total_clouds%30==0)
+            if(total_clouds%3==0)
                 cout << (float)total_clouds/argc * 100 << " percent complete..." << endl;
 
             //calculate the transform of the board
             Eigen::Matrix4f transform;
-            int found = getChessBoardPose(current,6,9,.0172,transform);
+            int found = getChessBoardPose(tmp,6,9,.0172,transform);
             transforms.push_back(transform);
             if(found){
                 success_mask.push_back(1); //indicate that the checkerboard was found
@@ -220,25 +321,50 @@ int main(int argc, char*argv[]){
         Eigen::Vector3f x = fitPlaneToTransformTranslations(transforms,success_mask); //get the parameters of the best fit plane to the checkerboard translations
         projectTransformTranslationsToPlane(x,&transforms,success_mask); //project the translations onto the plane
 
+        pcl::PointCloud<pcl::PointNormal>::Ptr master1 (new pcl::PointCloud<pcl::PointNormal>);
         for(int i = 0; i<clouds.size(); i++)
         {
             if(success_mask[i]!=0){
                 Eigen::Matrix4f transform = getSmoothedTransform(transforms,i,0.6,0,success_mask); //exponential smooth of the transforms - 0 means no smoothing
-                cout << "Transform: " << transform(0,3) << " " << transform(1,3) << " " << transform(2,3) << " " << endl; //print the transform for debuging
+                
+                //print some information about the transform for debugging
+                Eigen::Matrix3f rotation;
+                rotation <<     transform(0,0),transform(0,1),transform(0,2),
+                                transform(1,0),transform(1,1),transform(1,2),
+                                transform(2,0),transform(2,1),transform(2,2);
+                Eigen::Vector3f x(1,0,0);
+                Eigen::Vector3f y(0,1,0);
+                Eigen::Vector3f z(0,0,1);
+                x = rotation*x;
+                y = rotation*y;
+                z = rotation*z;
+                cout << "Transform: " << transform(0,3) << " " << transform(1,3) << " " << transform(2,3) << " " << endl;
+                cout << "X: " << x(0) << " " << x(1) << " " << x(2) << endl;
+                cout << "Y: " << y(0) << " " << y(1) << " " << y(2) << endl;
+                cout << "Z: " << z(0) << " " << z(1) << " " << z(2) << endl;
+
+
+
 
                 //transform the cloud and concatenate it with "master"
-                ColorCloudPtr c (new ColorCloud(clouds[i]));
-                pcl::transformPointCloud(*c,*c,transform);
-                for(int i = 0; i<c->points.size(); i++){
-                    pcl::PointXYZRGB p1 = c->points[i];
-                    ColorPoint p2;
-                    p2.x = p1.x;
-                    p2.y = p1.y;
-                    p2.z = p1.z;
-                    p2.r = p1.r;
-                    p2.g = p1.g;
-                    p2.b = p1.b;
-                    master->push_back(p2);
+                pcl::PointCloud<pcl::PointNormal>::Ptr c (new pcl::PointCloud<pcl::PointNormal>(clouds[i]));
+                pcl::transformPointCloudWithNormals(*c,*c,transform);
+                
+                for(int j = 0; j<c->points.size(); j++){
+                    pcl::PointNormal p1 = c->points[j];
+                    master1->push_back(p1);
+                }
+            }
+        }
+        for(int i = 0; i< clouds.size(); i++){
+            pcl::PointCloud<pcl::PointNormal>::Ptr c (new pcl::PointCloud<pcl::PointNormal>(clouds[i]));
+            Eigen::Matrix4f transform;
+            cout << "HERE" << endl;
+            if(getTransformForICPAlignment(master1,c,&transform)){
+                pcl::transformPointCloudWithNormals(*c,*c,transform);
+                for(int j = 0; j<c->points.size(); j++){
+                    pcl::PointNormal p1 = c->points[j];
+                    master->push_back(p1);
                 }
             }
         }
@@ -255,11 +381,12 @@ int main(int argc, char*argv[]){
         cout << "Found checkerboard in " << found_boards << " of the " << total_clouds << " clouds provided - (" << percent << "%)." << endl;
         //filter the remaining "master" cloud
 
+
         //filter by location
         cout << "Filtering the master cloud by relative position to the checkboard..." << endl;
 
         //strip out points not in the correct region
-        pcl::PassThrough<ColorPoint> pass;
+        pcl::PassThrough<pcl::PointNormal> pass;
         pass.setInputCloud (master);
         pass.setFilterFieldName ("z");
         pass.setFilterLimits (VERTICAL_LOWER_BOUND, 1.0);
@@ -278,9 +405,11 @@ int main(int argc, char*argv[]){
         pass.filter (*filtered);
         *master = *filtered;
 
+        pcl::io::savePCDFileASCII ("intermediate.pcd", *master);
+
         //filter by statical outliers
         cout << "Filtering the master cloud for statistical outliers..." << endl;
-        pcl::StatisticalOutlierRemoval<ColorPoint> sor;
+        pcl::StatisticalOutlierRemoval<pcl::PointNormal> sor;
         sor.setInputCloud (master);
         sor.setMeanK (70);
         sor.setStddevMulThresh (3.0);
@@ -289,23 +418,21 @@ int main(int argc, char*argv[]){
         
         //downsample the cloud
         cout << "Downsampling the cloud..." << endl;
-        float leaf_size = 0.002f;
         std::vector<int> index;
         pcl::removeNaNFromPointCloud(*master,*filtered,index);
-        pcl::VoxelGrid<ColorPoint> vg;
+        pcl::VoxelGrid<pcl::PointNormal> vg;
         vg.setInputCloud (master);
-        vg.setLeafSize (leaf_size,leaf_size,leaf_size);
+        vg.setLeafSize (LEAF_SIZE,LEAF_SIZE,LEAF_SIZE);
         vg.filter (*filtered);
         *master = *filtered;
 
-        pcl::io::savePCDFileASCII ("intermediate2.pcd", *master);
 
         //do a movingleastsquares smooth of the points
         cout << "Reconstructing..." << endl;
-        pcl::search::KdTree<ColorPoint>::Ptr tree (new pcl::search::KdTree<ColorPoint>);
-        pcl::PointCloud<ColorPoint> mls_points;
+        pcl::search::KdTree<pcl::PointNormal>::Ptr tree (new pcl::search::KdTree<pcl::PointNormal>);
+        pcl::PointCloud<pcl::PointNormal> mls_points;
         pcl::PointCloud<pcl::PointNormal>::Ptr mls_normals (new pcl::PointCloud<pcl::PointNormal>);
-        pcl::MovingLeastSquares<ColorPoint, pcl::PointNormal> mls;
+        pcl::MovingLeastSquares<pcl::PointNormal, pcl::PointNormal> mls;
         mls.setInputCloud (master);
         mls.setPolynomialFit (true);
         mls.setSearchMethod (tree);
@@ -314,15 +441,6 @@ int main(int argc, char*argv[]){
         mls.reconstruct (mls_points);
         *master = mls_points;
 
-        //compute the normals of the points
-        pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
-        pcl::NormalEstimation<ColorPoint, pcl::Normal> ne;
-        ne.setInputCloud (master);
-        pcl::search::KdTree<ColorPoint>::Ptr tree2 (new pcl::search::KdTree<ColorPoint>);
-        ne.setSearchMethod (tree2);
-        ne.setRadiusSearch (0.01);
-        ne.compute (*normals);
-        
         /*
         //filter by statical outliers
         cout << "Filtering the master cloud for statistical outliers..." << endl;
@@ -335,11 +453,10 @@ int main(int argc, char*argv[]){
         
         //close the bottom of the cloud to make a semi inclosed surface
         cout << "Closing the bottom of the shape..." << endl;
-        closeCloud(master);
+//        closeCloud(master);
 
         //save the output to a file
-        pcl::io::savePCDFileASCII ("test.pcd", *master);
-        pcl::io::savePCDFileASCII ("normals.pcd", *normals);
+        pcl::io::savePCDFileASCII ("output.pcd", *master);
     }
     return (0);
 }
